d1807c1734b0864bd6891f76cae1edcf8cc8d007
rlespinasse <romain.lespinasse@gmail.com>
2021-10-03 11:34:54 +0200

    fix(setup): make script award of its location

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -z "$GITHUB_ACTION_PATH" ]; then
+  GITHUB_ACTION_PATH=.
+fi
+
 echo "+ Create package.json file"
 echo '{"description": "","private": true,"version": "0.0.0","release":{}}' >package.json
 # shellcheck disable=SC2094
@@ -12,7 +16,7 @@ if [ "${INPUT_DRYRUN}" == "true" ]; then
 else
   echo "+ Setup releasable branches"
   # shellcheck disable=SC2094
-  cat <<<"$(jq --argjson branches "$(<branches.json)" '.release += {branches: $branches}' package.json)" >package.json
+  cat <<<"$(jq --argjson branches "$(<${GITHUB_ACTION_PATH}/branches.json)" '.release += {branches: $branches}' package.json)" >package.json
 fi
 
 if [ "${INPUT_WITHOUTPREFIX}" == "true" ]; then
