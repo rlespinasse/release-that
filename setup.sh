e19ce6a19f142d3d6682e87fb51421c090ca62bb
Romain Lespinasse <romain.lespinasse@gmail.com>
2024-10-20 09:07:34 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -4,23 +4,28 @@ if [ -z "$GITHUB_ACTION_PATH" ]; then
   GITHUB_ACTION_PATH=.
 fi
 
+CONFIG_PATH=.
+if [ -n "$INPUT_WORKINGDIRECTORY" ]; then
+  CONFIG_PATH="$INPUT_WORKINGDIRECTORY"
+fi
+
 echo "+ Create package.json file"
-echo '{"description": "","private": true,"version": "0.0.0","release":{}}' >package.json
+echo '{"description": "","private": true,"version": "0.0.0","release":{}}' >"$CONFIG_PATH/package.json"
 # shellcheck disable=SC2094
-cat <<<"$(jq --arg name "${GITHUB_REPOSITORY_NAME_PART}" '. += {name: $name}' package.json)" >package.json
+cat <<<"$(jq --arg name "${GITHUB_REPOSITORY_NAME_PART}" '. += {name: $name}' "$CONFIG_PATH/package.json")" >"$CONFIG_PATH/package.json"
 
 if [ "${INPUT_DRYRUN}" == "true" ]; then
   echo "+ Setup current branch as releasable (dry-run mode)"
   # shellcheck disable=SC2094
-  cat <<<"$(jq --arg branch "${GITHUB_REF_NAME}" '.release += {branches: [$branch]}' package.json)" >package.json
+  cat <<<"$(jq --arg branch "${GITHUB_REF_NAME}" '.release += {branches: [$branch]}' "$CONFIG_PATH/package.json")" >"$CONFIG_PATH/package.json"
 else
   echo "+ Setup releasable branches"
   # shellcheck disable=SC2094
-  cat <<<"$(jq --argjson branches "$(<"${GITHUB_ACTION_PATH}/branches.json")" '.release += {branches: $branches}' package.json)" >package.json
+  cat <<<"$(jq --argjson branches "$(<"${GITHUB_ACTION_PATH}/branches.json")" '.release += {branches: $branches}' "$CONFIG_PATH/package.json")" >"$CONFIG_PATH/package.json"
 fi
 
 if [ "${INPUT_WITHOUTPREFIX}" == "true" ]; then
   echo "+ Remove prefix from released version"
   # shellcheck disable=SC2094
-  cat <<<"$(jq '.release += {tagFormat: "${version}"}' package.json)" >package.json
+  cat <<<"$(jq '.release += {tagFormat: "${version}"}' "$CONFIG_PATH/package.json")" >"$CONFIG_PATH/package.json"
 fi
